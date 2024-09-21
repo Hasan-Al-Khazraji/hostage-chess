84f0f601fa2dca712306f3de7718c7ebdda8b4f3
Hasan Al-Khazraji <halkhazr@uoguelph.ca>
2024-09-20 21:06:13 -0400

    Header file init

---
+++ hclib.h
@@ -0,0 +1,19 @@
+typedef char board_t[8][8];
+/* row - rank; 0-7 corresponods to 1-8
+ * column - file; 0-7 corresponds to a-h
+ */
+typedef struct
+{
+    char bprison[16];
+    char bairfield[16];
+    board_t board;
+    char wprison[16];
+    char wairfield[16];
+} exboard_t;
+typedef struct
+{
+    int from_i, from_j;
+    int to_i, to_j;
+    char promotion;
+    char hostage;
+} move_t;
