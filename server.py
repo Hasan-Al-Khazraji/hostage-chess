195fb27e46b35ce5207fdcc4ed9af52241415c28
Hasan Al-Khazraji <halkhazr@uoguelph.ca>
2024-10-22 00:47:58 -0400

    Create a server file that handles all GET and POST requests

---
+++ server.py
@@ -0,0 +1,159 @@
+import sys; # used to get argv
+import cgi; # used to parse Mutlipart FormData 
+from http.server import HTTPServer, BaseHTTPRequestHandler;
+from urllib.parse import urlparse, parse_qsl;
+import hclib;
+
+
+# handler for our web-server - handles both GET and POST requests
+class MyHandler( BaseHTTPRequestHandler ):
+    def do_GET(self):
+        # parse the URL to get the path and form data
+        parsed  = urlparse(self.path);
+
+        # check if the web-pages matches the list
+        if parsed.path in [ '/start.html' ]:
+
+            # retreive the HTML file
+            content = """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Chessboard Integration</title>
+                    <!-- Chessboard.js CSS -->
+                    <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" integrity="sha384-q94+BZtLrkL1/ohfjR8c6L+A6qzNH9R2hBLwyoAfu3i/WCvQjzL2RQJ3uNHDISdU" crossorigin="anonymous">
+                    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
+                    <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js" integrity="sha384-8Vi8VHwn3vjQ9eUHUxex3JSN/NFqUg3QbPyX8kWyb93+8AC/pPWTzj+nHtbC5bxD" crossorigin="anonymous"></script>
+                </head>
+                <body>
+                        <div id="board1" style="width: 400px"></div>
+
+                    <script>
+                        var board1 = Chessboard('board1', 'start')
+                    </script>
+                    </body>
+                </html>
+            """
+
+            # generate the headers
+            self.send_response( 200 ); # OK
+            self.send_header( "Content-type", "text/html" );
+            self.send_header( "Content-length", len( content ) );
+            self.end_headers();
+
+            # send it to the broswer
+            self.wfile.write( bytes( content, "utf-8" ) );
+
+        # serve upload.html
+        elif parsed.path in [ '/upload.html' ]:
+
+            # retreive the HTML file
+            content = """
+            <html>
+            <head>
+                <title> Shoot! </title>
+            </head>
+            <body>
+                <form action="/display.html" method="post" enctype="multipart/form-data">
+                <input type="file" id="board" name="stringboard.txt">A
+                <input type="submit">
+                </form>
+            </body>
+            </html>
+            """
+
+            # generate the headers
+            self.send_response( 200 ); # OK
+            self.send_header( "Content-type", "text/html" );
+            self.send_header( "Content-length", len( content ) );
+            self.end_headers();
+
+            # send it to the broswer
+            self.wfile.write( bytes( content, "utf-8" ) );
+            
+        # IMAGES FROM START, THIS TOOK SO LONG
+        elif parsed.path[:5] == '/img/':
+            # retrieve the image file
+            fp = open('.' + self.path, 'rb')
+            content = fp.read()
+
+            # generate the headers
+            self.send_response(200)  # OK
+            self.send_header("Content-type", "image/png")
+            self.send_header("Content-length", len(content))
+            self.end_headers()
+
+            # send it to the browser
+            self.wfile.write(content)
+            fp.close()
+
+        else:
+            # generate 404 for GET requests that aren't the 3 files above
+            self.send_response( 404 );
+            self.end_headers();
+            self.wfile.write( bytes( "404: %s not found" % self.path, "utf-8" ) );
+
+
+    def do_POST(self):
+        parsed = urlparse(self.path)
+        
+        if parsed.path in [ '/display.html' ]:
+            # get the form data
+            form = cgi.FieldStorage( fp=self.rfile,
+                                     headers=self.headers,
+                                     environ = { 'REQUEST_METHOD': 'POST',
+                                                 'CONTENT_TYPE': 
+                                                   self.headers['Content-Type'],
+                                               } 
+                                   );
+        
+            # read file and convert to FEN
+            theBoard = form['stringboard.txt'].file.read().decode("utf-8");
+            theNewBoard = hclib.boardstring(theBoard);
+            fen_string = hclib.fen(theNewBoard, 'w', 'KQkq', '-', 0, 1);
+            
+            # read the HTML file and insert the form data
+            content = f"""
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Chessboard</title>
+                <!-- Chessboard.js CSS -->
+                <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" integrity="sha384-q94+BZtLrkL1/ohfjR8c6L+A6qzNH9R2hBLwyoAfu3i/WCvQjzL2RQJ3uNHDISdU" crossorigin="anonymous">
+                <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
+                <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js" integrity="sha384-8Vi8VHwn3vjQ9eUHUxex3JSN/NFqUg3QbPyX8kWyb93+8AC/pPWTzj+nHtbC5bxD" crossorigin="anonymous"></script>
+            </head>
+            <body>
+                <div id="myBoard" style="width: 400px"></div>
+
+                <script>
+                    var board = Chessboard('myBoard', '{fen_string}');
+                </script>
+            </body>
+            </html>
+            """
+
+            # generate the headers
+            self.send_response( 200 ); # OK
+            self.send_header( "Content-type", "text/html" );
+            self.send_header( "Content-length", len( content ) );
+            self.end_headers();
+
+            # send it to the browser
+            self.wfile.write( bytes( content, "utf-8" ) );
+
+        else:
+            # generate 404 for POST requests that aren't the file above
+            self.send_response( 404 );
+            self.end_headers();
+            self.wfile.write( bytes( "404: %s not found" % self.path, "utf-8" ) );
+
+
+if __name__ == "__main__":
+    httpd = HTTPServer( ( 'localhost', int(sys.argv[1]) ), MyHandler );
+    print( "Server listing in port:  ", int(sys.argv[1]) );
+    httpd.serve_forever();
